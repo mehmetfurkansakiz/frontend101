606f092a8db1c639917c86ce4f1ec126252b32ef
mehmetfurkansakiz <62005335+Furkanium@users.noreply.github.com>
2022-07-29 21:47:31 +0300

    Synchronized

---
+++ js/027-form-calismak-bolum-sonu-egzersizi.js
@@ -0,0 +1,42 @@
+let userFormDOM = document.querySelector('#userForm')
+userFormDOM.addEventListener('submit', formHandler)
+const alertDOM = document.querySelector('#alert')
+
+const alertFunction = (title, message, className ="warning") => `
+<div class="alert alert-${className} alert-dismissible fade show" role="alert">
+  <strong>${title}</strong> ${message}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+`
+function formHandler(event){
+    event.preventDefault()
+    const USER_NAME = document.querySelector("#username")
+    const SCORE = document.querySelector("#score")
+    if (USER_NAME.value && SCORE.value){
+        addItem(USER_NAME.value , SCORE.value)
+        USER_NAME.value = ""
+        SCORE.value = ""
+    }else {
+        alertDOM.innerHTML = alertFunction(
+            "Baslik Bilgisi",
+            "Eksik Bilgi Girdiniz",
+            "danger" //hata //more notifications settings (succes,danger)
+            )
+    }
+}
+
+/*
+<li class="list-group-item d-flex justify-content-between align-items-center">
+A list item
+<span class="badge bg-primary rounded-pill">14</span>
+</li>
+*/
+let userListDOM = document.querySelector('#userList')
+
+const addItem = (userName, score) => {
+    let liDOM = document.createElement('li')
+    liDOM.innerHTML = `${userName}
+    <span class="badge bg-primary rounded-pill">${score}</span>`
+    liDOM.classList.add('list-group-item' , 'd-flex' , 'justify-content-between' , 'align-items-center')
+    userListDOM.append(liDOM)
+}
