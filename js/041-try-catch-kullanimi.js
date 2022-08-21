28bdfd0162735f82c1cc2cab172a85f973c7bf51
mehmetfurkansakiz <62005335+Furkanium@users.noreply.github.com>
2022-08-21 19:06:09 +0300

    Synchronized

---
+++ js/041-try-catch-kullanimi.js
@@ -0,0 +1,18 @@
+let items
+
+
+try 
+{
+    items.forEach(product => {
+        console.log(product)
+    });
+} catch (error) 
+{
+    console.log("error: ",error)
+}
+
+console.log("hata yonetimi duzgun calisiyor")
+
+let info ="mehmetfurkansakiz"
+
+console.log(info)
---
+++ js/042-fetch-api-calismak.js
@@ -0,0 +1,29 @@
+fetch("data/settings.json").then(response => {
+        return response.json()
+    }
+).then(responseJson => {
+    console.log(responseJson)
+    console.log(responseJson.userName)
+})
+
+const userListDOM = document.querySelector('#userList')
+
+//API üzerinden veri cektik
+fetch("https://jsonplaceholder.typicode.com/posts").then
+(
+    response => response.json()
+)
+.then(responseJson => 
+{
+    responseJson.forEach(item => {
+        // console.log(item) //forEach ile hepsini tek tek yazdirdik
+        let liDOM = document.createElement('li')
+        liDOM.innerHTML = item.title
+        userListDOM.append(liDOM)
+    });
+})
+
+fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json()).then(responseJson => {
+    console.log(responseJson.slice(4,8))
+})
+
